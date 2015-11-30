adfe15f2fe8849d650f2da89552420eb50fb5a55
Li Zhi <zhi_li@itp.co.jp>
2015-11-30 17:27:59 +0900

    Add sumSelfList function.

--- sumSelf.hs
+++ sumSelf.hs
@@ -3,3 +3,6 @@ import qualified Data.List as List
 sumSelf :: [Int] -> Map.Map Int Int -> Map.Map Int Int
 sumSelf [] sums = sums
 sumSelf (x:ns) sums = List.foldl' (\m n ->if Map.member (x+n) m then m else Map.insert (x+n) (x+n) m) (sumSelf ns sums) (x:ns)
+
+sumSelfList :: [Int] -> [Int]
+sumSelfList ns = Map.keys $ sumSelf ns Map.empty
