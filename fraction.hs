3301252680e3bc48cfddfc7c059f9488292995f8
Li Zhi <zhi_li@itp.co.jp>
2015-12-01 17:43:21 +0900

    problem 27

--- fraction.hs
+++ fraction.hs
@@ -1,4 +1,17 @@
-fraction :: Int -> Int -> [Int]
-fraction n d = if n `mod` d == 0
-               then [n `div` d]
-               else (n `div` d) : (fraction ((n `mod` d)*10) d) 
+import Data.List
+
+fraction :: Int -> Int -> [Int] -> [(Int, Int)]
+fraction n d a = if n `mod` d == 0
+                 then [(n `div` d, -1)]
+                 else
+                   if (n `mod` d) `elem` a
+                   then [(n `div` d, unboxMaybe $ elemIndex (n `mod` d) a)]
+                   else (n `div` d, -1) : (fraction ((n `mod` d)*10) d ((n `mod` d):a)) 
+
+unboxMaybe :: Maybe Int -> Int
+unboxMaybe Nothing = 0
+unboxMaybe (Just n) = n
+
+cyclesLen :: Int -> Int -> Int
+cyclesLen a d = let frac = fraction a d []
+                in (snd $ frac !! (length frac - 1)) + 1
