8732560e53f1e3bc304d358a2f9832eece1c91c8
智 李 <lizhi1977@hotmail.com>
2016-01-21 00:35:58 +0900

    add some test about monad.

--- condiv.hs
+++ condiv.hs
@@ -1,18 +1,27 @@
 -- problem 47
 import Data.List
+import qualified Data.Map as Map
 
 divisors :: Int -> [Int]
 divisors n = let cs = [1..floor $ sqrt (fromIntegral n :: Double)]
              in concat $ map (\x -> if n `mod` x == 0 then [x, n `div` x] else []) cs
 
-primeFactors' :: Int -> [Int]
-primeFactors' n = if divisors n == [1, n] 
-                 then [n]
-                 else concat $ map primeFactors $ filter (\x -> x /= 1 && x/=n) $ divisors n
-                 where n' = fromIntegral n
+mergeResult :: [([Int], Map.Map Int [Int])] -> ([Int], Map.Map Int [Int])
+mergeResult a = foldl (\(accx, accy) (x, y) -> (x ++ accx, Map.union y accy)) ([], Map.empty) a
 
-primeFactors :: Int -> [Int]
-primeFactors n = (map head . group . sort) $ primeFactors' n
+primeFactors' :: (Int, Map.Map Int [Int]) -> ([Int], Map.Map Int [Int])
+primeFactors' (n, m) = if Map.member n m 
+                       then (Map.findWithDefault [] n m, m)
+                       else if divisors n == [1, n] 
+                            then ([n], Map.insert n [n] m)
+                            else let a = map (\f -> primeFactors' (f, m)) $ filter (\x -> x /= 1 && x/=n) $ divisors n
+                                 in mergeResult a
+                            where n' = fromIntegral n
+
+primeFactors :: (Int, Map.Map Int [Int]) -> ([Int], Map.Map Int [Int])
+primeFactors (n, m) = let (n1, m1) = primeFactors' (n, m)
+                          n2 = (map head . group . sort) n1
+                      in (n2, Map.insert n n2 $ Map.union m m1)
 
 primeCount :: Int -> Int
-primeCount n = length $ primeFactors n
+primeCount n = length $ primeFactors (n, Map.empty)
